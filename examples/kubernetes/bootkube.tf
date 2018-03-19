d9bba178063aaec3ba69fcd50dad0dea40fc39eb
Riad Vargas de Oliveira <riad@walltime.info>
2018-03-19 01:05:41 -0300

    Fixing git source syntax. This syntax is not working in newest versions of Terraform (e.g. v0.11.4).

--- examples/kubernetes/bootkube.tf
+++ examples/kubernetes/bootkube.tf
@@ -1,5 +1,5 @@
 module "bootkube" {
-  source = "git://https://github.com/poseidon/terraform-render-bootkube.git?ref=v0.9.1"
+  source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=v0.9.1"
 
   cluster_name                  = "${var.cluster_name}"
   api_servers                   = ["${var.cluster_name}-api.${var.k8s_domain_name}"]
