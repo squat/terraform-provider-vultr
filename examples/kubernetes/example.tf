e9ebacf9082616a7760dde5ec8e741cafed1ce61
Lucas Serven <lserven@gmail.com>
2018-08-17 18:26:50 +0200

    examples/kubernetes: point to typhoon-vultr

---
+++ examples/kubernetes/example.tf
@@ -0,0 +1,37 @@
+resource "vultr_dns_domain" "example" {
+  domain = "example.com"
+  ip     = "0.0.0.0"
+}
+
+module "typhoon" {
+  source = "git::https://github.com/squat/typhoon-vultr?ref=7373f6292c25172054f934575180b5cc3f1cad88"
+
+  cluster_name = "example"
+
+  # Vultr
+  region          = "${data.vultr_region.frankfurt.id}"
+  dns_zone        = "${vultr_dns_domain.example.domain}"
+  controller_type = "${data.vultr_plan.2gb.id}"
+  worker_type     = "${data.vultr_plan.2gb.id}"
+
+  # configuration
+  ssh_authorized_key = "${file("/path/to/ssh/public/key")}"
+  asset_dir          = "assets"
+
+  # optional
+  worker_count = 2
+}
+
+data "vultr_region" "frankfurt" {
+  filter {
+    name   = "name"
+    values = ["Frankfurt"]
+  }
+}
+
+data "vultr_plan" "2gb" {
+  filter {
+    name   = "ram"
+    values = ["2048"]
+  }
+}
