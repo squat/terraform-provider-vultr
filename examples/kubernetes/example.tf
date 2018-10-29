92291aa79642528fe01af38441d57d18e80007a8
Lucas Serven <lserven@gmail.com>
2018-10-29 19:42:57 +0100

    examples/kubernetes: bump to 1.12.2

--- examples/kubernetes/example.tf
+++ examples/kubernetes/example.tf
@@ -4,7 +4,7 @@ resource "vultr_dns_domain" "example" {
 }
 
 module "typhoon" {
-  source = "git::https://github.com/squat/typhoon-vultr?ref=v1.11.3"
+  source = "git::https://github.com/squat/typhoon-vultr?ref=v1.12.2"
 
   cluster_name = "example"
 
