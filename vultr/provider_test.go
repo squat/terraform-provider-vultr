3c708120b3e83e53aaf037510a78a8d7cb6dd2e5
Fady Farid <ffarid@vultr.com>
2019-02-15 10:55:27 -0500

    update provider_test.go to be in line with what official Terraform providers use

--- vultr/provider_test.go
+++ vultr/provider_test.go
@@ -1,16 +1,35 @@
 package vultr
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 )
 
-var _ terraform.ResourceProvider = Provider()
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider().(*schema.Provider)
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"vultr": testAccProvider,
+	}
+}
 
 func TestProvider(t *testing.T) {
 	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
-		t.Fatalf("expected provider to validate: %v", err)
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("VULTR_API_KEY"); v == "" {
+		t.Fatal("VULTR_API_KEY must be set for acceptance tests")
 	}
 }
