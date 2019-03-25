86c174748ff0b423ed703160eceb5705e199eb58
Fady Farid <ffarid@vultr.com>
2019-03-25 12:32:59 -0400

    add firewall rule acceptance test

--- vultr/resource_firewall_rule_test.go
+++ vultr/resource_firewall_rule_test.go
@@ -1,8 +1,109 @@
 package vultr
 
 import (
+	"fmt"
+	"strings"
 	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
 )
+
+func TestAccVultrFirewallRule_basic(t *testing.T) {
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckVultrFirewallRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVultrFirewallRuleConfigBasic(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckVultrFirewallRuleExists("vultr_firewall_rule.bar"),
+					resource.TestCheckResourceAttr("vultr_firewall_rule.bar", "protocol", "tcp"),
+					resource.TestCheckResourceAttr("vultr_firewall_rule.bar", "cidr_block", "0.0.0.0/0"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckVultrFirewallRuleDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "vultr_firewall_rule" {
+			continue
+		}
+
+		firewallGroupID, id, err := parseStringSlashInt(rs.Primary.ID, "firewall rule ID", "firewall-group-id", "firewall-rule-number")
+		if err != nil {
+			return err
+		}
+
+		client := testAccProvider.Meta().(*Client)
+		firewallRules, err := client.GetFirewallRules(firewallGroupID)
+		if err != nil {
+			if strings.HasPrefix(err.Error(), "Invalid firewall group") {
+				// firewall group and rule were destroyed by Terraform cleanup
+				return nil
+			}
+			return fmt.Errorf("Error getting firewall rule (%s): %v", firewallGroupID, err)
+		}
+
+		for _, f := range firewallRules {
+			if f.RuleNumber == id {
+				return fmt.Errorf("Firewall rule was not deleted")
+			}
+		}
+
+		return nil
+	}
+	return nil
+}
+
+func testAccCheckVultrFirewallRuleExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		firewallGroupID, id, err := parseStringSlashInt(rs.Primary.ID, "firewall rule ID", "firewall-group-id", "firewall-rule-number")
+		if err != nil {
+			return err
+		}
+		client := testAccProvider.Meta().(*Client)
+		firewallRules, err := client.GetFirewallRules(firewallGroupID)
+		if err != nil {
+			return fmt.Errorf("Error retrieving firewall rules: %v", err)
+		}
+
+		for _, f := range firewallRules {
+			if f.RuleNumber == id {
+				return nil
+			}
+		}
+
+		return fmt.Errorf("Firewall rule does not exist")
+	}
+}
+
+func testAccVultrFirewallRuleConfigBasic(rInt int) string {
+	return fmt.Sprintf(`
+		resource "vultr_firewall_group" "foo" {
+			description = "foo-%d"
+		}
+
+		resource "vultr_firewall_rule" "bar" {
+			firewall_group_id = "${vultr_firewall_group.foo.id}"
+			cidr_block        = "0.0.0.0/0"
+			protocol          = "tcp"
+			from_port         = 22
+			to_port           = 22
+		}
+	`, rInt)
+}
 
 func TestSplitFirewallRule(t *testing.T) {
 	cases := []struct {
