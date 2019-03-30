1ba2997e1e8ef96c1d35f4503d9d4aeac5a20a90
Lucas Servén Marín <lserven@gmail.com>
2019-03-30 23:02:55 +0100

    examples: bump Kubernetes to v1.13.5

--- examples/kubernetes/example.tf
+++ examples/kubernetes/example.tf
@@ -4,7 +4,7 @@ resource "vultr_dns_domain" "example" {
 }
 
 module "typhoon" {
-  source = "git::https://github.com/squat/typhoon-vultr?ref=v1.13.4"
+  source = "git::https://github.com/squat/typhoon-vultr?ref=v1.13.5"
 
   cluster_name = "example"
 
