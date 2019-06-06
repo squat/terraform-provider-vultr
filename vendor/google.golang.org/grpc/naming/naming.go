0b8b6e5251bdb80587c278ec4422c92a7970af6e
Joseph Mulloy <github@joe.mulloy.me>
2019-06-06 01:54:30 +0000

    Update to terraform 0.12 via glide update

--- vendor/google.golang.org/grpc/naming/naming.go
+++ vendor/google.golang.org/grpc/naming/naming.go
@@ -17,9 +17,8 @@
  */
 
 // Package naming defines the naming API and related data structures for gRPC.
-// The interface is EXPERIMENTAL and may be suject to change.
 //
-// Deprecated: please use package resolver.
+// This package is deprecated: please use package resolver instead.
 package naming
 
 // Operation defines the corresponding operations for a name resolution change.
@@ -30,7 +29,7 @@ type Operation uint8
 const (
 	// Add indicates a new address is added.
 	Add Operation = iota
-	// Delete indicates an exisiting address is deleted.
+	// Delete indicates an existing address is deleted.
 	Delete
 )
 
